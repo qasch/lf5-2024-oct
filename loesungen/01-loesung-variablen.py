e242cb5c7f8a95eedc41abb7c9e7aeac789d22f0
Kai Schell <kai.schell@oscert.eu>
2024-10-14 16:03:28 +0200

    add loesungen

---
+++ loesungen/01-loesung-variablen.py
@@ -0,0 +1,50 @@
+# Aufgabe 1:
+
+x = 10
+y = 5
+
+summe = x + y
+
+print(summe)
+
+
+# Aufgabe 2:
+
+my_float = 3.14
+my_text = "Hallo, Welt!"
+my_boolean = True
+
+type_of_my_float = type(my_float)  # Rückgabe von type() in Variable speichern
+print(type_of_my_float)
+print(type(my_float)) # gleiche Ausgabe wie oben
+print(type(my_text))
+print(type(my_boolean))
+
+
+# Aufgabe 3:
+
+name = "Gretl"
+alter = 11
+wohnort = "Spiekershausen"
+
+print("Ich bin", name, "ich bin", alter, "Jahre alt und wohne in", wohnort)
+
+
+# Aufgabe 4:
+
+zahl1 = 15
+zahl2 = 4
+
+# Addition
+addition = zahl1 + zahl2
+subtraktion = zahl1 - zahl2
+mulitplikation = zahl1 * zahl2
+division = zahl1 / zahl2
+modulo = zahl1 % zahl2
+
+print(addition)
+print(subtraktion)
+print(mulitplikation)
+print(division)
+print(modulo)
+
---
+++ loesungen/02-loesung-benutzereingaben.py
@@ -0,0 +1,26 @@
+# Lösungen zu Aufgaben zur Benutzereingabe
+
+# Aufgabe 1
+
+# name = input("Bitte gib deinen Namen ein: ")
+# alter = input("Bitte gib dein Alter ein: ")
+# stadt = input("Bitte gib deine Stadt ein: ")
+
+# print("Mein Name ist " + name + " ich bin " + alter + " Jahre alt.")
+
+# Aufgabe 2
+
+temperatur_in_celsius = float(input("Bitte gib eine Temperatur in Celsius ein: "))
+temperatur_in_fahrenheit = (temperatur_in_celsius * 9/5) + 32
+
+print(temperatur_in_celsius, "Grad Celsius sind ", temperatur_in_fahrenheit, "Grad in Fahrenheit.")
+
+
+# Aufgabe 3
+
+# zahl1 = int(input("Bitte gib eine Zahl ein: "))
+# zahl2 = int(input("Bitte gib eine weitere Zahl ein: "))
+
+# summe = zahl1 + zahl2 
+
+# print("Das Ergebnis ist ", summe)
